e34836f2a6523c9aefc1e41f97d29fba997d7a50
Mirco Pergreffi <mircopergreffi@gmail.com>
2024-08-17 21:10:30 +0200

    Added Bottles List

---
+++ bottles.h
@@ -0,0 +1,37 @@
+#ifndef BOTTLES_H
+#define BOTTLES_H
+
+const char* PYD_bottles[] =
+{
+  "Vuoto",
+  "Bourbon Whiskey",
+  "Brandy",
+  "Cachaca",
+  "Coca Cola",
+  "Cognac",
+  "Gin",
+  "Ginger Beer",
+  "Lager",
+  "Lemon Soda",
+  "Limonata",
+  "Mezcal",
+  "Prosecco",
+  "Rum Bianco",
+  "Rum Scuro",
+  "Rye Whiskey",
+  "Sciroppo Di Lampone",
+  "Sciroppo Di Zucchero",
+  "Scotch Whisky",
+  "Soda",
+  "Succo D'Ananas",
+  "Succo D'Arancia",
+  "Succo Di Lime",
+  "Succo Di Limone",
+  "Succo Di Pompelmo",
+  "Tequila",
+  "Tonica",
+  "Triple Sec",
+  "Vodka",
+};
+
+#endif /* BOTTLES_H */
