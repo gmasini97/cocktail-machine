7f56d06c402d44cf69a56617ffa0b6fb41b39c8b
Mirco Pergreffi <mircopergreffi@gmail.com>
2024-08-25 10:33:33 +0000

    Fixed brackets formatting

--- pyd_servo.cpp
+++ pyd_servo.cpp
@@ -1,21 +1,25 @@
 #include "pyd_servo.h"
 
-void PYD_Servo::begin() {
+void PYD_Servo::begin()
+{
     mServo.attach(mServoPin);
     mServo.setPeriodHertz(50);
 }
 
-void PYD_Servo::end() {
+void PYD_Servo::end()
+{
     mServo.detach();
 }
 
-bool PYD_Servo::move(int pos) {
+bool PYD_Servo::move(int pos)
+{
     if (pos < 0 || pos > 180)
         return false;
     mServo.write(pos);
     return true;
 }
 
-int PYD_Servo::position() {
+int PYD_Servo::position()
+{
     return mServo.read();
 }
