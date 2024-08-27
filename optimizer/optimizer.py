45bf2f0d6d6648c89aaf99a7d306f189cdd40b65
Mirco Pergreffi <mircopergreffi@gmail.com>
2024-08-27 16:57:08 +0000

    Find combination of bottles that maximize the number of preparable cocktails

---
+++ optimizer/optimizer.py
@@ -0,0 +1,33 @@
+import json
+import numpy as np
+
+def ingredientsAvailable(recipe, ingredients):
+    for x in recipe['ingredients']:
+        if 'ingredient' not in x:
+            continue
+        if x['ingredient'] not in ingredients:
+            return False
+    return True
+
+with open('recipes.json', encoding='utf8') as f:
+    recipes = json.load(f)
+    ingredients_raw = [x['ingredient'] for recipe in recipes for x in recipe['ingredients'] if 'ingredient' in x]
+    ingredients = list(set(ingredients_raw))
+    ingredients.sort()
+    counts = [ingredients_raw.count(ingredient) for ingredient in ingredients]
+
+    ingredients = np.array(ingredients)
+    probs = counts/np.sum(counts)
+
+    bestCombination = []
+    bestCombinationCount = 0
+
+    while True:
+        combination = np.random.choice(ingredients, size=8, replace=False, p=probs)
+        count = [ingredientsAvailable(recipe, combination) for recipe in recipes].count(True)
+        if count > bestCombinationCount:
+            bestCombination = combination
+            bestCombinationCount = count
+            print(bestCombination, bestCombinationCount)
+            if bestCombinationCount == len(recipes):
+                break
